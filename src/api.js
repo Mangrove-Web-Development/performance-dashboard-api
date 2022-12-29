f9afeefdb6c97cf15767c210bb2917c8feac5862
aykan <aykanburcak@gmail.com>
2022-12-29 15:51:09 +0300

    Notion api route

--- src/api.js
+++ src/api.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const {Client} = require('@notionhq/client');
+const dotenv = require('dotenv');
+dotenv.config();
 
+const notion = new Client({auth: process.env.NOTION_API_KEY});
 const app = express();
 const router = express.Router();
 
+async function getReports(filter, sorts) {
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_REPORTS_DATABASE_ID,
+      filter,
+      sorts,
+    });
+    return response.results;
+  } catch (error){
+    console.log(error.body);
+  }
+}
+
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+  getReports().then(response => {
+    res.status(200).json(response);
+  }).catch(error => { return res.send(error) });
 });
 
 app.use(`/.netlify/functions/api`, router);
